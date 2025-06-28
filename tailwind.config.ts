8f28a8d93bc91bfc61c0febebbcc0d8e61e1b2b1
Aamir217 <mohd23506@gmail.com>
2025-06-29 02:32:53 +0530

    Added Dark Theme

---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        colors: {
+            background: 'hsl(var(--background))',
+            foreground: 'hsl(var(--foreground))',
+            card: {
+                DEFAULT: 'hsl(var(--card))',
+                foreground: 'hsl(var(--card-foreground))'
+            },
+            popover: {
+                DEFAULT: 'hsl(var(--popover))',
+                foreground: 'hsl(var(--popover-foreground))'
+            },
+            primary: {
+                DEFAULT: 'hsl(var(--primar))',
+                foreground: 'hsl(var(--primary-foreground))'
+            },
+            secondary: {
+                DEFAULT: 'hsl(var(--secondary))',
+                foreground: 'hsl(var(--secondary-foreground))'
+            },
+            muted: {
+                DEFAULT: 'hsl(var(--muted))',
+                foreground: 'hsl(var(--muted-foreground))'
+            }
+        },
+    },
+  },
+  plugins: [],
+};
