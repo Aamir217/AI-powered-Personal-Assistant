847d2236c892885a4a109d4f122589e5e4be05d8
Aamir217 <mohd23506@gmail.com>
2025-07-04 16:52:14 +0530

    Add Header and some navigations

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,30 +1,34 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import { AuthContext } from '@/context/AuthContext';
 import { api } from '@/convex/_generated/api';
 import { GetAuthUserData } from '@/services/GlobalApi';
 import { useGoogleLogin } from '@react-oauth/google';
 import { useMutation } from 'convex/react';
 import Image from 'next/image'
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useContext } from 'react'
 function SignIn() {
     
 const CreateUser=useMutation(api.users.CreateUser);
+const {user,setUser} = useContext(AuthContext);
+const router = useRouter();
 const googleLogin = useGoogleLogin({
   onSuccess: async (tokenResponse) => {
-    console.log(tokenResponse);
     if(typeof window!== undefined){
         localStorage.setItem('user_token',tokenResponse.access_token);
     }
     
    const user=await GetAuthUserData(tokenResponse.access_token);
-    console.log(user);
     //Save User Info
     const result = await CreateUser({
         name:user?.name,
         email:user?.email,
         picture:user?.picture
     });
-    console.log("--",result);
+    setUser(result);
+    console.log(user);
+    router.replace('/ai-assistants')
   },
   onError: errorResponse => console.log(errorResponse),
 });
---
+++ app/(main)/ai-assistants/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AIAssistants() {
+  return (
+    <div>
+      AIAssistants
+    </div>
+  )
+}
+
+export default AIAssistants
